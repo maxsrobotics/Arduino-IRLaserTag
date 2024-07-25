cc0adb2b9261b4844f96b57bbffb86f81b5be264
maxsrobotics <maxsrobotics@gmail.com>
2024-07-25 12:50:53 -0400

    [[BROKEN]] more work for sending many hex values at once

--- Arduino-IRLaserTag.ino
+++ Arduino-IRLaserTag.ino
@@ -1,29 +1,27 @@
-// Arduino IR Laser Tag Protocol - Custom Protocol
-
 #include <IRremote.h>
 
 const int irSend = 9;
 const int irRecv = 8;
 
-int shotArray[] = {
+const uint16_t shotArray[] = {
   0xAA, 0x24
 };
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  
   IrSender.begin(irSend);
   IrReceiver.begin(irRecv);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  IrSender.sendNECRaw(shotArray, 0);
+  for(int i = 0; i < sizeof(shotArray) / sizeof(uint16_t); i++) {
+    IrSender.sendNECRaw(shotArray[i]);
+    delay(50);
+  }
 
-  if(IrReceiver.decode()) {
-    //Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
-    IrReceiver.printIRResultShort(&Serial);
+  if (IrReceiver.decode()) {
+    Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
+    IrReceiver.resume(); // Prepare to receive the next value
   }
   delay(1000);
 }
